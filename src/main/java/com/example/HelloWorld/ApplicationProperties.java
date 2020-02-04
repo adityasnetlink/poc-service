e575f37776aed05c085a12c79b01881667a2a610
aditya <aditya@gmail.com>
2020-02-04 23:33:32 +0530

    replaced pomsss

--- src/main/java/com/example/HelloWorld/ApplicationProperties.java
+++ src/main/java/com/example/HelloWorld/ApplicationProperties.java
@@ -1,5 +1,5 @@
 package com.example.HelloWorld;
-import lombok.Data;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
