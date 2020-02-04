4ad10a9d4f495452d47162a40035ae6359b4d7a3
aditya <aditya@gmail.com>
2020-02-04 23:32:12 +0530

    1

--- src/main/java/com/example/HelloWorld/Scheduler.java
+++ src/main/java/com/example/HelloWorld/Scheduler.java
@@ -2,12 +2,14 @@ package com.example.HelloWorld;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
+	  @Autowired
+        private ApplicationProperties applicationProperties;
 
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
 			"MM/dd/yyyy HH:mm:ss");
@@ -17,6 +19,9 @@ public class Scheduler {
 
 		System.out.println("Regular task performed at "
 				+ dateFormat.format(new Date()));
+				System.out.println("The balance url is " + this.applicationProperties.getGetbalance());
+            System.out.println("The transfer url is " + this.applicationProperties.getTransferfund());
+            System.out.println("The customerinfo url is " + this.applicationProperties.getCustomerinfo());
 
 	}
 
