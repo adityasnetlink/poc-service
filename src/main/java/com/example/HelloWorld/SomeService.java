bf1bd6d8e868b1395df5a3646f00115f8c81f359
aditya <adityas@netlink.com>
2020-02-05 10:58:28 +0530

    new1dd

--- src/main/java/com/example/HelloWorld/SomeService.java
+++ src/main/java/com/example/HelloWorld/SomeService.java
@@ -1,13 +1,12 @@
 package com.example.HelloWorld;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class SomeService {
 
-	@Autowired
-	private ApplicationProperties applicationProperties;
+	
 
 	@Value("${netlink}")
 	private String userBucketPath;
@@ -15,14 +14,7 @@ public class SomeService {
 	public void hello() {
 		System.out.println("Hello from scheduler");
 		System.out.println(userBucketPath);
-		/*
-		 * System.out.println("The balance url is " +
-		 * this.applicationProperties.getGetbalance());
-		 * System.out.println("The transfer url is " +
-		 * this.applicationProperties.getTransferfund());
-		 * System.out.println("The customerinfo url is " +
-		 * this.applicationProperties.getCustomerinfo());
-		 */
+		
 	}
 
 }
