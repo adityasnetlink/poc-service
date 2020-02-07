6edcfd5d212ee7f119fc038701fb48b47471f584
aditya <adityas@netlink.com>
2020-02-07 14:21:47 +0530

    ressdffss

---
+++ src/main/java/com/HelloWorld/WelcomeConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.HelloWorld;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "welcome")
+@Configuration
+public class WelcomeConfiguration {
+
+    private String message = "Welcome to Netlink";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
