36ee192891a58932e2dbd2b8f8db168daa315ad1
Aditya Sharma <adityasharma@Ayushs-Mac-mini.local>
2020-03-03 10:56:22 +0530

    new changes1

--- src/main/java/com/HelloWorld/SpringCloudKubernetesConfigExampleApplication.java
+++ src/main/java/com/HelloWorld/SpringCloudKubernetesConfigExampleApplication.java
@@ -2,10 +2,12 @@ package com.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@RefreshScope
 public class SpringCloudKubernetesConfigExampleApplication {
 
 	public static void main(String[] args) {
