05d79f8d013bf642d412dcd184db5da7d987a8b5
Aditya Sharma <adityasharma@Ayushs-Mac-mini.local>
2020-03-03 11:21:36 +0530

    new changes4

--- src/main/java/com/PocService/SpringCloudKubernetesConfigExampleApplication.java
+++ src/main/java/com/PocService/SpringCloudKubernetesConfigExampleApplication.java
@@ -1,6 +1,7 @@
 package com.PocService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 @RefreshScope
+@EnableAutoConfiguration
 public class SpringCloudKubernetesConfigExampleApplication {
 
 	public static void main(String[] args) {
