9a61dd37105e675b8a36c3616aaa028f322c11e6
adityasnetlink <54260549+adityasnetlink@users.noreply.github.com>
2020-07-29 18:59:44 +0530

    Update WelcomeController.java

--- src/main/java/com/PocService/WelcomeController.java
+++ src/main/java/com/PocService/WelcomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/welcome")
 @RestController
-@RefreshScope
+//@RefreshScope
 public class WelcomeController {
 
     @Value("${message:Welcome to Youtube}")
