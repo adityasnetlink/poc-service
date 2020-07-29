0e84ec65d71fd7cd515d00d4aab667b6d57ae925
adityasnetlink <54260549+adityasnetlink@users.noreply.github.com>
2020-07-29 12:18:42 +0530

    Update WelcomeConfiguration.java

--- src/main/java/com/PocService/WelcomeConfiguration.java
+++ src/main/java/com/PocService/WelcomeConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "welcome")
+@RefreshScope
 @Configuration
 
 public class WelcomeConfiguration {
