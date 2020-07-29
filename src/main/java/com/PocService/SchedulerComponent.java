c1f9487555aa82ae001bfb24fb4fb1733da2ecbb
adityasnetlink <54260549+adityasnetlink@users.noreply.github.com>
2020-07-29 13:17:55 +0530

    Update SchedulerComponent.java

--- src/main/java/com/PocService/SchedulerComponent.java
+++ src/main/java/com/PocService/SchedulerComponent.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 @Component
 @RefreshScope
+@Configuration
 public class SchedulerComponent {
 
 	
